8f8b5b1c46e13ad601e28b1fa88157b8c621b2f4
mariomastrandrea <mariomastrandrea@mbp-di-mario.homenet.telecomitalia.it>
2021-04-12 10:19:11 +0200

    mia versione di regine
    calcola un vettore contenente tutte le possibili soluzioni

---
+++ src/main/java/it/polito/tdp/regine/model/Regine.java
@@ -0,0 +1,84 @@
+package it.polito.tdp.regine.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Regine
+{
+	private int N;
+	private List<Integer[]> soluzioni;
+	
+	
+	public List<Integer[]> risolvi(int N) 
+	{
+		this.N = N;
+		List<Integer[]> soluzioni = new ArrayList<>();
+		
+		List<Integer> soluzioneParziale = new ArrayList<>();
+		cerca(soluzioneParziale, 0);
+		
+		return this.soluzioni;
+	}
+	
+	//scacchiera N x N
+	//righe e colonne numerate da 0 a N-1
+	//ad ogni livello posizioniamo una regina in una nuova riga
+	//soluzione parziale: lista delle colonne in cui mettere le regine (prime righe)
+	//	List<Integer>
+	// livello = quante righe sono gia' piene
+	// livello = 0 => nessuna riga piena
+	// livello = 3 => righe piene (0, 1, 2), devo mettere la regina nella riga 3
+	// [0]
+	// 		[0, 2]
+	// 				[0, 2, 1] ....
+	
+	//cerca == true : trovato; cerca == false : cerca ancora
+	private void cerca(List<Integer> parziale, int livello)
+	{
+		if(livello == N)
+		{
+			this.soluzioni.add(parziale.toArray(new Integer[N]));
+		}
+		else 
+		{
+			for(int colonna=0; colonna<N; colonna++)
+			{
+				//if la mossa nella casella [livello][colonna] e' valida
+				// se sì, aggiungi a parziale e fai ricorsione
+				
+				if(posizioneValida(parziale,colonna))
+				{
+					parziale.add(colonna);	//[0, 6, 4, 6, XXX]
+					cerca(parziale, livello+1);
+					
+					parziale.remove(parziale.size()-1); //backtracking
+				}
+			}
+		}
+	}
+	
+	private boolean posizioneValida(List<Integer> soluzioneParziale, int colonnaNuovaRegina)
+	{
+		if(soluzioneParziale.contains(colonnaNuovaRegina))
+			return false;
+		
+		//controllo le diagonali: confronto la posizione (livello, colonna) con (r,c)
+		//delle regine esistenti
+		//Osservazione: le celle sulle diagonali inclinate a pi/4 hanno somma (riga+colonna) costante;
+		//				le celle sulle diagonali inclinate a 3pi/4 hanno differenza (riga-colonna) costante;
+		
+		int rigaNuovaRegina = soluzioneParziale.size();
+		
+		for(int riga = 0; riga < rigaNuovaRegina; riga++)
+		{
+			int colonna = soluzioneParziale.get(riga);
+			
+			if( rigaNuovaRegina+colonnaNuovaRegina == riga+colonna ||
+				rigaNuovaRegina-colonnaNuovaRegina == riga-colonna )
+				return false; 			//la nuova regina è in conflitto con una diagonale 										
+		}								//già occupata da un'altra regina precedente
+		return true;
+	}
+	
+	
+}
---
+++ src/main/java/it/polito/tdp/regine/model/TestRegine.java
@@ -0,0 +1,19 @@
+package it.polito.tdp.regine.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TestRegine
+{
+
+	public static void main(String[] args)
+	{
+		Regine r = new Regine();
+		
+		List<Integer> soluzione = new ArrayList<>();
+		r.risolvi(8);
+				
+	
+	}
+
+}
